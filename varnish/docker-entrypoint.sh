9c2010653cbff3feddefcf75b8dd1a3e08000866
Etienne Dauvergne <contact@ekyna.com>
2016-12-10 14:04:50 +0100

    Improved varnish image (varnishd in foreground / removed varnishlog)

--- varnish/docker-entrypoint.sh
+++ varnish/docker-entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
+set -e
 
 # TODO args/cmd
 
 if [[ ! -f /var/lib/varnish/secret ]]
 then
     cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > /var/lib/varnish/secret
-    #chown nobody /var/lib/varnish/secret
 fi
 
-varnishd -a 0.0.0.0:${VARNISH_PORT} \
-         -f /etc/varnish/default.vcl \
-         -p vcc_allow_inline_c=on \
-         -S /var/lib/varnish/secret \
-         -s malloc,256M
+if [[ "$1" = "start" ]]
+then
+    varnishd -a 0.0.0.0:${VARNISH_PORT} \
+        -f /etc/varnish/default.vcl \
+        -p vcc_allow_inline_c=on \
+        -S /var/lib/varnish/secret \
+        -s malloc,${VARNISH_MEMORY} \
+        -F
+fi
 
-sleep 1
-varnishlog
+exec "$@"
